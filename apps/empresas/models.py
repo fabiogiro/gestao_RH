fa12bf6a69742c0228925dcc12593dea0092f88d
Fabio Giro <fabio_giro@yahoo.com.br>
2020-01-30 10:15:53 -0300

    Criando a app Funcionarios

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Empresa', blank=False, null=False)
+
+    def __str__(self):
+        return self.nome
